52c64751a755601d8821a91e5ac93467e6fd9838
Michal Vojáček <mvojacekdev@gmail.com>
2024-11-26 03:38:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ sed -i "s|\$UPSTREAM|${UPSTREAM}|" /etc/nginx/nginx.conf
 sed -i "s|\$GZIP|${GZIP:-on}|" /etc/nginx/nginx.conf
 sed -i "s|\$ALLOWED_ORIGIN|${ALLOWED_ORIGIN:-*}|" /etc/nginx/nginx.conf
 sed -i "s|\$PROXY_READ_TIMEOUT|${PROXY_READ_TIMEOUT:-120s}|" /etc/nginx/nginx.conf
+sed -i "s|\$LISTEN|${LISTEN:-80}|" /etc/nginx/nginx.conf
 
 if [[ "${PROXY_CACHE_VALID+x}" ]]; then
   PROXY_CACHE_VALID="proxy_cache_valid ${PROXY_CACHE_VALID};"
